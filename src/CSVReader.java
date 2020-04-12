a2b8d4f78604b6e4e10c468f32052d2ff4731165
linh-phamdang21 <ph.danglinh214@gmail.com>
2020-04-12 18:26:09 +0700

    Add files

---
+++ src/CSVReader.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CSVReader {
+    public static void main(String[] args) {
+        String csvFile = "country.csv";
+        BufferedReader br = null;
+        String line = "";
+        String csvSplitBy = ",";
+
+        try {
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null){
+                String[] country = line.split(csvSplitBy);
+                System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
+            }
+        }  catch (IOException e){
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
